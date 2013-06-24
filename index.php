f66076cdcce76363da8ed7845904652033fa7a0b
Eugene Esca <scsmash3r@gmail.com>
2013-06-24 13:54:10 +0300

    Index file minor changes
    - Minor changes in index.php file: keeping it clean and tidy, also
    because of new installation process - added a check for installer
    removal and empty config file in case if user abandoned installation
    process at the very middle.

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
 define('IN_EZRPG', true);
+define('CUR_DIR', realpath(dirname(__FILE__)));
 
-if (!file_exists('config.php')) {
+// Checking if config.php was created, and if it has some data, so we might be sure the user is not abandoned the process at the very middle
+if (!file_exists(CUR_DIR.'/config.php') or filesize(CUR_DIR.'/config.php') == 0) {
 	header('Location: install.php');
 	exit();
 }
 
-require_once 'init.php';
+// Deleting installer, if everything is already set
+if (isset($_GET['act']) && $_GET['act'] == 'deleteinstaller' && file_exists(CUR_DIR.'/install.php')) {
+	unlink(CUR_DIR.'/install.php');
+	header('Location: index.php');
+	exit();
+}
+
+// Main skeleton
+
+require_once CUR_DIR.'/init.php';
 
 $default_mod = 'Index';
 
