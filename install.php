32e2040f3d22ee85157a4e8a5f670c77607db9c9
Eugene Esca <scsmash3r@gmail.com>
2013-06-24 13:55:25 +0300

    New installation process
    - Reverted back to one-file installer, it was unnecessary to make it
    modular o_O 99% of users and developers won't change there anything.
    They actually need a working base to extend from, and old installer
    simply did that nicely.
    
    - Redo some code. Added HTML5 shiv for IE browsers and changed overall
    installer style (YUI Pure CSS). Still, no images or anything using -
    keeping it simple at installation process.

--- install.php
+++ install.php
@@ -5,10 +5,22 @@ define('CUR_DIR', realpath(dirname(__FILE__)));
 // Including a library to help generate random secret key
 include './lib/func.rand.php';
 
+// Perform a check, if user abandoned a process on the file permission checking and config creation
+$install_step = $_GET['act'];
+
+if (!file_exists(CUR_DIR.'/config.php') && !$install_step) {
+	header('Location: install.php?act=2');
+}
+
+// Performing a check for last step and install file removal
+if ($install_step == 5) {
+	header('Location: index.php?act=deleteinstaller');
+}
 
 // Here goes the installation header template
 function displayHeader() {
 
+global $install_step;
 echo <<<HEAD
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -22,27 +34,31 @@ echo <<<HEAD
 	body,html{
 		height:100%;
 	}
+	
+	a {
+		color:#f20;
+	}
 
 	#content {
 		min-height:100%;
 		height:100%;
-		background: #a90329; /* Old browsers */
-		background: -moz-linear-gradient(top,  #a90329 0%, #8f0222 44%, #6d0019 100%); /* FF3.6+ */
-		background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#a90329), color-stop(44%,#8f0222), color-stop(100%,#6d0019)); /* Chrome,Safari4+ */
-		background: -webkit-linear-gradient(top,  #a90329 0%,#8f0222 44%,#6d0019 100%); /* Chrome10+,Safari5.1+ */
-		background: -o-linear-gradient(top,  #a90329 0%,#8f0222 44%,#6d0019 100%); /* Opera 11.10+ */
-		background: -ms-linear-gradient(top,  #a90329 0%,#8f0222 44%,#6d0019 100%); /* IE10+ */
-		background: linear-gradient(to bottom,  #a90329 0%,#8f0222 44%,#6d0019 100%); /* W3C */
-		filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a90329', endColorstr='#6d0019',GradientType=0 ); /* IE6-9 */
+		background: #a90329;
+		background: -moz-linear-gradient(top,  #a90329 0%, #8f0222 44%, #6d0019 100%);
+		background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#a90329), color-stop(44%,#8f0222), color-stop(100%,#6d0019));
+		background: -webkit-linear-gradient(top,  #a90329 0%,#8f0222 44%,#6d0019 100%);
+		background: -o-linear-gradient(top,  #a90329 0%,#8f0222 44%,#6d0019 100%);
+		background: -ms-linear-gradient(top,  #a90329 0%,#8f0222 44%,#6d0019 100%);
+		background: linear-gradient(to bottom,  #a90329 0%,#8f0222 44%,#6d0019 100%);
+		filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a90329', endColorstr='#6d0019',GradientType=0 );
 	}
 	
 	#main {
 		width:500px;
-		height:300px;
+		height:500px;
 		position:absolute;
 		left:50%;
 		top:50%;
-		margin:-350px 0 0 -300px;
+		margin:-430px 0 0 -300px;
 		background-color: #FFFFFF;
 		padding:50px;
 		font-size:12px;
@@ -62,19 +78,19 @@ echo <<<HEAD
         }
 
         .pure-button-success {
-            background: rgb(28, 184, 65); /* this is a green */
+            background: rgb(28, 184, 65);
         }
 
         .pure-button-error {
-            background: rgb(202, 60, 60); /* this is a maroon */
+            background: rgb(202, 60, 60);
         }
 
         .pure-button-warning {
-            background: rgb(223, 117, 20); /* this is an orange */
+            background: rgb(223, 117, 20);
         }
 
         .pure-button-secondary {
-            background: rgb(66, 184, 221); /* this is a light blue */
+            background: rgb(66, 184, 221);
         }
 	
 	</style>
@@ -86,7 +102,7 @@ echo <<<HEAD
 
 	<div id="content">
 	<div id="main">
-	<h1>ezRPG Installation</h1>
+	<h1>ezRPG Installation - Step {$install_step}</h1>
 	
 
 HEAD;
@@ -94,20 +110,21 @@ HEAD;
 }
 
 // The installation footer template
-function displayFooter()
-{
-    echo <<<FOOT
+function displayFooter() {
+
+echo <<<FOOT
 </div>
 </div>
 </body>
 </html>
 FOOT;
+
 }
 
 displayHeader();
 
 // Step 1 - initial stage with greetings and some brief info about engine
-if (!isset($_GET['act'])) {
+if (empty($install_step)) {
         echo "<p>Hello there, noble sir!<br /><br /> It seems like you've just downloaded a fresh <strong>\"simple\"</strong> version of <strong>ezRPG</strong>!<br /> This one package counts as a legacy one, but it's a good start-off for an RPG developers!<br /> If you have any questions or support for this engine, please visit <a href='http://ezrpgproject.net/' target='_blank'>ezRPG Official Site</a></p>";
         echo "<p>In this version you will find only basic stuff to start your own RPG with. If you're seeking for more advanced version, check out GitHub repos for new versions.</p>";
 		echo "<p>For now, you need to create a <strong>configuration file</strong> for your engine and connect it with DB.</p>";
@@ -115,11 +132,11 @@ if (!isset($_GET['act'])) {
 }
 
 // Step 2 - checking DIR permissions and creating configuration file
-else if ($_GET['act'] == '2') {
+else if ($install_step == 2) {
 
 		// The check itself
 		$files = array();
-		if (is_writable(CUR_DIR.'/config.php')){
+		if (!is_writable(CUR_DIR.'/config.php')){
 			$fh = fopen(CUR_DIR.'/config.php', 'w+');
 			if(!$fh){
 				$files[] = CUR_DIR."/config.php";
@@ -127,56 +144,61 @@ else if ($_GET['act'] == '2') {
 			fclose($fh);
 		}
 		
-		if (is_writable(CUR_DIR.'/tpl/cache/')){
+		if (!is_writable(CUR_DIR.'/tpl/cache/')){
 			$files[] = CUR_DIR."/tpl/cache/";
 		}
 		
+		if (!is_writable(CUR_DIR.'/install.php')){
+			$files[] = CUR_DIR."/install.php";
+		}
+		
 		if (!empty($files)){
-				echo '<strong>The following file(s) need to be writable:</strong><br />';
+				echo '<strong>The following file(s) and folders need to be writable (CHMOD 777):</strong><br />';
 				foreach($files as $file){
 					echo '<p>'.$file.'</p>';
 				}
 				echo "<a href='install.php?act=2' class='pure-button pure-button-error'>Check again</a>";
 		} else {
-				echo "<p>Good news, sir! All files have needed permissions! We can move on now to the next step.";
-				echo "<a href='install.php?act=3' class='pure-button pure-button-success'>Setup DB information</a>.";
+				echo "<p>Good news, sir! All files have needed permissions! We can move on now to the next step.</p>";
+				echo "<a href='install.php?act=3' class='pure-button pure-button-success'>Setup DB information</a>";
 		}
-	return;
-    echo '<h2>Step 2</h2>';
-    
-    if (!isset($_POST['submit']))
-    {
+}
+
+// Step 3 - setting up database and filling it with data
+else if ($install_step == 3) {
+
+    if (!isset($_POST['submit'])) {
+		
+		// This is the initial values, filled in by default
         $dbhost = 'localhost';
         $dbname = 'ezrpg';
         $dbuser = '';
         $dbpass = '';
-        $dbprefix = '';
-    }
-    else
-    {
+        $dbprefix = 'ezrpg_';
+		
+    } else {
+	
+		// Here we are checking user entered values for errors
         $errors = 0;
         $msg = '';
         
-        if (isset($_POST['dbhost']) && empty($_POST['dbhost']))
-        {
+        if (isset($_POST['dbhost']) && empty($_POST['dbhost'])) {
             $errors = 1;
             $msg .= 'You need to enter a host name!<br />';
         }
-        if (isset($_POST['dbname']) && empty($_POST['dbname']))
-        {
+		
+        if (isset($_POST['dbname']) && empty($_POST['dbname'])) {
             $errors = 1;
             $msg .= 'You need to enter a database name!<br />';
         }
-        if (isset($_POST['dbuser']) && empty($_POST['dbuser']))
-        {
+		
+        if (isset($_POST['dbuser']) && empty($_POST['dbuser'])) {
             $errors = 1;
             $msg .= 'You need to enter a database user!<br />';
         }
         
-        //so far so good...
-        if ($errors == 0)
-        {
-            //let's test the connection
+        if ($errors == 0) {
+            // Let's test the connection
             $db = mysql_connect($_POST['dbhost'], $_POST['dbuser'], $_POST['dbpass']);
             if (!$db)
             {
@@ -194,16 +216,16 @@ else if ($_GET['act'] == '2') {
             }
         }
         
-        if ($errors == 0)
-        {
-            //No problesm connecting and selecting the database
-            //Save details to the config file and fill the database
+        if ($errors == 0) {
+		
+            // Seems that there is no problems with DB connection, so move on and save the config file + fill the DB
             $dbhost = $_POST['dbhost'];
             $dbname = $_POST['dbname'];
             $dbuser = $_POST['dbuser'];
             $dbpass = $_POST['dbpass'];
             $dbprefix = $_POST['dbprefix'];
-            //fill the database first
+			
+            // First to fill the DB. You can change the initial values here, but better to do it via phphMyAdmin or etc, after the fill in process
             $query1 = <<<QUERY
 CREATE TABLE IF NOT EXISTS `{$dbprefix}players` (
   `id` int(11) unsigned NOT NULL auto_increment,
@@ -234,7 +256,7 @@ CREATE TABLE IF NOT EXISTS `{$dbprefix}players` (
   PRIMARY KEY  (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 QUERY;
             mysql_query($query1) or die('Something went wrong.');
             
@@ -248,7 +270,7 @@ CREATE TABLE IF NOT EXISTS `{$dbprefix}player_log` (
   PRIMARY KEY  (`id`),
   KEY `player_log` (`player`,`time`),
   KEY `new_logs` (`player`,`status`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 QUERY;
             mysql_query($query2) or die('Something went wrong.');
             
@@ -262,25 +284,20 @@ QUERY;
 defined('IN_EZRPG') or exit;
 
 /*
-  Title: Config
+  This is the configuration file.
   The most important settings for the game are set here.
 */
 
 /*
-  Variables: Database Connection
-  Connection settings for the database.
-  
-  \$config_server - Database server
-  \$config_dbname - Database name
-  \$config_username - Username to login to server with
-  \$config_password - Password to login to server with
-  \$config_driver - Contains the database driver to use to connect to the database.
+  Here goes your DB connection info.
+  You can change it at any time here.
 */
-\$config_server = '{$dbhost}';
-\$config_dbname = '{$dbname}';
-\$config_username = '{$dbuser}';
-\$config_password = '{$dbpass}';
-\$config_driver = 'mysql';
+
+\$config_server = '{$dbhost}'; // Database server
+\$config_dbname = '{$dbname}'; // Database name
+\$config_username = '{$dbuser}'; // Username to login to server with
+\$config_password = '{$dbpass}'; // Password to login to server with
+\$config_driver = 'mysql'; // Contains the database driver to use to connect to the database.
 
 /*
   Constant:
@@ -290,35 +307,27 @@ defined('IN_EZRPG') or exit;
   This should only ever be set ONCE! Any changes to it will cause your game to break!
   You should save a copy of the key on your computer, just in case the secret key is lost or accidentally changed,.
   
-  SECRET_KEY - A long string of random characters.
+  SECRET_KEY - A long string of random characters. It is already randomly generated only for your instance of ezRPG.
 */
+
 define('SECRET_KEY', '{$secret_key}');
 
-
 /*
-  Constants: Settings
-  Various settings used in ezRPG.
-  
-  DB_PREFIX - Prefix to the table names
-  VERSION - Version of ezRPG
-  SHOW_ERRORS - Turn on to show PHP errors.
-  DEBUG_MODE - Turn on to show database errors and debug information.
+  Here goes various settings, that are used in ezRPG.
 */
-define('DB_PREFIX', '{$dbprefix}');
-define('VERSION', '1.0');
+
+define('DB_PREFIX', '{$dbprefix}'); // Prefix to the table names
+define('VERSION', '1.0'); // Version of ezRPG
+define('PACKAGE', 'SIMPLE'); // Name of the package, since it is a custom build over legacy 1.0 version. This constant is needed for some modules compatibility check.
 define('SHOW_ERRORS', 0);
 define('DEBUG_MODE', 0);
 ?>
 CONF;
             file_put_contents('config.php', $config);
             echo '<p>Config file written.</p>';
-            echo '<p><a href="install.php?act=3">Continue to next step</a></p>';
-            displayFooter();
-            exit;
-        }
-        else
-        {
-            echo '<p><strong>Sorry, there were some problems:</strong><br />', $msg, '</p>';
+            echo '<p><a href="install.php?act=4" class="pure-button pure-button-success">Continue to next step</a></p>';
+        } else {
+            echo '<p><strong>Sorry, there were some problems:</strong><br /><span style="color:#f20">'.$msg.'</span></p>';
             
             $dbhost = $_POST['dbhost'];
             $dbname = $_POST['dbname'];
@@ -327,48 +336,56 @@ CONF;
             $dbprefix = $_POST['dbprefix'];
         }
     }
-    
-    echo '<p>Please fill in the database access details here.</p>';
-    echo '<form method="post" action="install.php?act=2">';
-    echo '<label>Host</label>';
-    echo '<input type="text" name="dbhost" value="', $dbhost, '" />';
-    echo '<label>Database Name</label>';
-    echo '<input type="text" name="dbname" value="', $dbname, '" />';
-    echo '<label>User</label>';
-    echo '<input type="text" name="dbuser" value="', $dbuser, '" />';
-    echo '<label>Password</label>';
-    echo '<input type="password" name="dbpass" value="', $dbpass, '" />';
-    echo '<label>Table Prefix (Optional)</label>';
-    echo '<input type="text" name="dbprefix" value="', $dbprefix, '" />';
-    echo '<p>You can enter a prefix for your table names if you like.<br />This can be useful if you will be sharing the database with other applications, or if you are running more than one ezRPG instance in a single database.</p>';
-    echo '<input type="submit" name="submit" value="Submit"  class="button" />';
-    echo '</form>';
-    displayFooter();
-    exit;
+
+    echo '<p>Please fill in the database access details here:</p>';
+	echo '<form class="pure-form pure-form-stacked" method="post" action="install.php?act='.$install_step.'">';
+echo <<<DBFORM
+		<fieldset>
+		
+			<label for="dbhost">Host</label>
+			<input id="dbhost" type="text" name="dbhost" value="{$dbhost}">
+			
+			<label for="dbname">Database Name</label>
+			<input id="dbname" type="text" name="dbname" value="{$dbname}">
+			
+			<label for="dbuser">User</label>
+			<input id="dbuser" type="text" name="dbuser" value="{$dbuser}">
+
+			<label for="password">Password</label>
+			<input id="password" type="password" name="dbpass" value="{$dbpass}">
+			
+			<label for="dbprefix">Table Prefix (optional)</label>
+			<input id="dbprefix" type="text" name="dbprefix" value="{$dbprefix}">
+			
+			<p>You can enter a prefix for your table names if you like.<br />This can be useful if you will be sharing the database with other applications, or if you are running more than one ezRPG instance in a single database.</p>
+		
+			<input type="submit" name="submit" class="pure-button pure-button-success" value="Submit">
+		</fieldset>
+DBFORM;
+	echo '</form>';
 }
 
-else if ($_GET['act'] == '3')
-{
-    displayHeader();
-    echo '<h1>Step 3</h1>';
-    
-    if (isset($_POST['submit']))
-    {
+// Step 4 - populate the DB, creating admin account
+else if ($install_step == 4) {
+
+    if (isset($_POST['submit'])) {
+	
         $errors = 0;
         $msg = '';
-        if (empty($_POST['username']) || empty($_POST['email']) || empty($_POST['password']) || empty($_POST['password']))
-        {
+		
+        if (empty($_POST['username']) || empty($_POST['email']) || empty($_POST['password']) || empty($_POST['password'])) {
             $errors = 1;
-            $msg .= 'You forgot to fill in something!';
+            $msg .= "You forgot to fill in something!";
         }
-        if ($_POST['password'] != $_POST['password2'])
-        {
+		
+        if ($_POST['password'] != $_POST['password2']) {
             $errors = 1;
-            $msg .= 'You didn\'t verify your password correctly.';
+            $msg .= "Passwords don't match.";
         }
         
-        if ($errors == 0)
-        {
+        if ($errors == 0) {
+		
+			// If there is no errors, we create administrator's account and deleting install.php file
             include 'config.php';
             mysql_connect($config_server, $config_username, $config_password);
             mysql_select_db($config_dbname);
@@ -376,34 +393,47 @@ else if ($_GET['act'] == '3')
             $secret_key = createKey(16);
             $query = 'INSERT INTO `' . DB_PREFIX . 'players` (`username`, `password`, `email`, `secret_key`, `registered`, `rank`) VALUES(\'' . mysql_real_escape_string($_POST['username']) . '\', \'' . mysql_real_escape_string(sha1($secret_key . $_POST['password'] . SECRET_KEY)) . '\', \'' . mysql_real_escape_string($_POST['email']) . '\', \'' . mysql_real_escape_string($secret_key) . '\', ' . time() . ', 10)';
             mysql_query($query);
-            
-            echo '<p>Your admin account has been created! You may now login to the game. You can access the admin panel at <em>/admin</em>.</p>';
-            echo '<p><strong>Please delete install.php immediately!</strong></p>';
-            echo '<p><a href="index.php">Visit your ezRPG!</a></p>';
-            displayFooter();
-            exit;
-        }
-        else
-        {
+			
+			echo "<p>Admin user has been successfully created!</p>";
+			echo "<p>Now all you need to do, kind sir, is to press that red button below. You won't be able to return to installation process anymore, since the <strong>install.php</strong> file will be deleted. You can always get a new one from <a href=\"https://github.com/scsmash3r/ezRPG-Simple\" target=\"_blank\">ezRPG Simple GitHub Repository</a>.</p>";
+			echo "<a href='install.php?act=5' class='pure-button pure-button-error'>Delete installer and proceed to ezRPG!</a>";
+			
+			displayFooter();
+			exit();
+			
+        } else {
+		
             echo '<p><strong>Sorry, there were some problems:</strong><br />', $msg, '</p>';
+			
         }
     }
     
     echo '<p>Create your admin account for ezRPG.</p>';
-    echo '<form method="post" action="install.php?act=3">';
-    echo '<label>Username</label>';
-    echo '<input type="text" name="username" value="', $_POST['username'], '" />';
-    echo '<label>Email</label>';
-    echo '<input type="text" name="email" value="', $_POST['email'], '" />';
-    echo '<label>Password</label>';
-    echo '<input type="password" name="password" />';
-    echo '<label>Verify Password</label>';
-    echo '<input type="password" name="password2" />';
-    echo '<br />';
-    echo '<input type="submit" value="Create" name="submit" class="button" />';
-    echo '</form>';
-    displayFooter();
-    exit;
+	
+	echo '<form class="pure-form pure-form-stacked" method="post" action="install.php?act='.$install_step.'">';
+echo <<<ADMFORM
+		<fieldset>
+		
+			<label for="username">Username</label>
+			<input id="username" type="text" name="username" value="{$_POST['username']}">
+			
+			<label for="email">Email</label>
+			<input id="email" type="text" name="email" value="{$_POST['email']}">
+			
+			<label for="password">Password</label>
+			<input id="password" type="password" name="password">
+			
+			<label for="password2">Verify password</label>
+			<input id="password2" type="password" name="password2">
+			
+			<p><strong>WARNING:</strong> Don't ever name your account "admin" - be more creative, try to choose your very own admin name.</p>
+		
+			<input type="submit" name="submit" class="pure-button pure-button-success" value="Submit">
+			
+		</fieldset>
+ADMFORM;
+	echo '</form>';
+	
 }
 
 displayFooter();
